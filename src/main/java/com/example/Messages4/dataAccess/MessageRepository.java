041aaa11fe854a0d672e42f96c4b1a2c70f78309
Bill Fairfield <bill@fairfieldco.com>
2023-11-11 16:24:22 -0500

    Update MessageRepository.java

--- src/main/java/com/example/Messages4/dataAccess/MessageRepository.java
+++ src/main/java/com/example/Messages4/dataAccess/MessageRepository.java
@@ -2,6 +2,7 @@ package com.example.Messages4.dataAccess;
 
 import com.example.Messages4.model.Message;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends CrudRepository<Message, Long> {
