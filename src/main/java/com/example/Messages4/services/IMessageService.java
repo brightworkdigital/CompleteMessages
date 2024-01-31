73ccd79f8bebd669cefa1555565970d92d58eab0
Dave <davedykes@davedykes.com>
2024-01-31 07:32:59 +0000

    Initial commit

--- src/main/java/com/example/Messages4/services/IMessageService.java
+++ src/main/java/com/example/Messages4/services/IMessageService.java
@@ -1,8 +1,6 @@
 package com.example.Messages4.services;
 
 import com.example.Messages4.model.Message;
-import org.springframework.stereotype.Service;
-
 
 public interface IMessageService {
     Iterable<Message> getAllMessages();
